20d5e967a2b56898d972f47ab71f6b657af77646
gatemediang <estolog@gmail.com>
2020-08-10 05:39:41 +0100

    Updates

---
+++ precache-manifest.3a2e25196f8998c2d7a2888cebfac3c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41bd6cf8f5454f1a5c7489587dd28240",
+    "url": "/HotDevJobs/index.html"
+  },
+  {
+    "revision": "5a736835bd3a1a9b968c",
+    "url": "/HotDevJobs/static/js/2.f1fe30c9.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/HotDevJobs/static/js/2.f1fe30c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0d705f48035416024a",
+    "url": "/HotDevJobs/static/js/main.62f615f7.chunk.js"
+  },
+  {
+    "revision": "6483813b1144c750f420",
+    "url": "/HotDevJobs/static/js/runtime-main.89b719dd.js"
+  }
+]);
